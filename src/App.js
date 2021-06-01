7ea78f5cc09d1638244502852278c84ace21c71f
Daniel <mdrmtz@gmail.com>
2021-06-01 13:13:35 -0700

    Arrow function.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ class App extends Component {
             monsters: [],
             searchField: ''
         };
-
-        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -20,7 +18,7 @@ class App extends Component {
             .then(users => this.setState({monsters:users}))
     }
 
-    handleChange(e) {
+    handleChange = (e) => {
         this.setState({searchField: e.target.value})
     }
 
